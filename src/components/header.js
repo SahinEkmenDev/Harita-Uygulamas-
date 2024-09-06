4c0dcc76d92f7c957353b266f4cb8bf16618fa4f
Şahin EKMEN <sahin.ekmen6868@gmail.com>
2024-09-07 00:30:13 +0300

    fix:headerjsAdded

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,8 @@
-import React, { useContext } from "react";
+// src/components/Header.js
+import React from "react";
 import "../header.css";
-import { AppContext } from "../context/appcontext";
 
 const Header = () => {
-  const { logout, user } = useContext(AppContext); // Context API'den user ve logout fonksiyonunu alıyoruz
-
   return (
     <header>
       <div className="container">
@@ -13,31 +11,18 @@ const Header = () => {
           <h1>Harita Uygulaması</h1>
         </div>
         <nav className="navbar">
-          {user ? (
-            // Kullanıcı giriş yapmışsa Logout butonu ve kullanıcı adını göster
-            <>
-              <span>Merhaba, {user.name}</span>
-              <button onClick={logout} className="btn">
-                Logout
-              </button>
-            </>
-          ) : (
-            // Kullanıcı giriş yapmamışsa diğer butonları göster
-            <>
-              <button id="addPointBtn" className="btn">
-                Add Point
-              </button>
-              <button id="queryBtn" className="btn">
-                Query
-              </button>
-              <button id="geometryBtn" className="btn">
-                Geometry
-              </button>
-              <button id="resetViewBtn" className="btn">
-                <i className="fas fa-home"></i>
-              </button>
-            </>
-          )}
+          <button id="addPointBtn" className="btn">
+            Add Point
+          </button>
+          <button id="queryBtn" className="btn">
+            Query
+          </button>
+          <button id="geometryBtn" className="btn">
+            Geometry
+          </button>
+          <button id="resetViewBtn" className="btn">
+            <i className="fas fa-home"></i>
+          </button>
         </nav>
       </div>
     </header>
